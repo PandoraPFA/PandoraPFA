2bb663ffdcf654470a5b87a809119f0a92c021c2
Peter Speckmayer <speckmay@cern.ch>
2009-08-26 10:09:28 +0000

    added TestCaloHitManager
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraPFANew/trunk@34 e861a63a-b6b5-47dc-a5de-b4304083765c

---
+++ include/Test/TestCaloHitManager.h
@@ -0,0 +1,69 @@
+/**
+ *	@file	PandoraPFANew/include/Test/TestCaloHitManager.h
+ * 
+ *	@brief	Header file for the CaloHitManager test-class.
+ * 
+ *	$Log: $
+ */
+#ifndef TEST_CALO_HIT_MANAGER_H
+#define TEST_CALO_HIT_MANAGER_H 1
+
+#include <assert.h>
+
+#include "BasicTypes.h"
+#include "StatusCodes.h"
+
+
+namespace pandora
+{
+	
+/**
+ *	@brief TestCaloHitManager class
+ */
+class TestCaloHitManager
+{
+public:
+	/**
+	 *	@brief	test the GetMCParticle method
+	 */
+	 StatusCode Test_CreateCaloHit();
+   
+	/**
+	 *	@brief	test the matching of calo hits to their correct mc particles for particle flow
+	 */
+	StatusCode Test_MatchCaloHitsToMCPfoTargets();
+
+	/**
+	 *	@brief	Test to get the calorimeter hit vector
+	 */	
+	StatusCode Test_GetCaloHitVector();
+
+	/**
+	 *	@brief	Test to get the current ordered calo hit vector
+	 * 
+	 */	
+	StatusCode Test_GetCurrentOrderedCaloHitVector();	
+
+	/**
+	 *	@brief	Reset the calo hit manager
+	 */	
+	StatusCode Test_Reset();
+
+	/**
+	 *	@brief	combined tests
+	 */
+	 StatusCode Test_Combined();
+
+	/**
+	 *	@brief	Run all tests
+	 */
+	 StatusCode Test_All();
+   
+
+
+
+};
+
+} // namespace pandora
+
+#endif
