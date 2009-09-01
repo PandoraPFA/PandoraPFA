f0fb85b5a12e68a40fe2666147d37cac4e6991e5
John Marshall <marshall@hep.phy.cam.ac.uk>
2009-09-01 17:28:41 +0000

    Added implementation of PandoraSettings class.
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraPFANew/trunk@48 e861a63a-b6b5-47dc-a5de-b4304083765c

---
+++ src/PandoraSettings.cc
@@ -0,0 +1,23 @@
+/**
+ *	@file PandoraPFANew/src/PandoraSettings.cc
+ * 
+ *	@brief Implementation of the pandora settings class.
+ * 
+ *	$Log: $
+ */
+
+#include "Xml/tinyxml.h"
+
+#include "PandoraSettings.h"
+
+namespace pandora
+{
+
+StatusCode PandoraSettings::Read(const TiXmlHandle *const pXmlHandle)
+{
+	// TODO read in pandora parameters here
+
+	return STATUS_CODE_SUCCESS;
+}
+	
+} // namespace pandora
