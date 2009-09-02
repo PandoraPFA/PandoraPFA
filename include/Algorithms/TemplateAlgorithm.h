d5261a6c3ac5f03785f947d25f15de0a04578ea0
John Marshall <marshall@hep.phy.cam.ac.uk>
2009-09-02 17:32:31 +0000

    Added TemplateAlgorithm. Removed tab characters from pandora settings xml file.
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraPFANew/trunk@55 e861a63a-b6b5-47dc-a5de-b4304083765c

---
+++ include/Algorithms/TemplateAlgorithm.h
@@ -0,0 +1,42 @@
+/**
+ *  @file   PandoraPFANew/include/Algorithms/TemplateAlgorithm.h
+ * 
+ *  @brief  Header file for the template algorithm class.
+ * 
+ *  $Log: $
+ */
+#ifndef TEMPLATE_ALGORITHM_H
+#define TEMPLATE_ALGORITHM_H 1
+
+#include "Algorithms/Algorithm.h"
+
+/**
+ *  @brief  TemplateAlgorithm class
+ */
+class TemplateAlgorithm : public pandora::Algorithm
+{
+public:
+    /**
+     *  @brief  Factory class for instantiating algorithm
+     */
+    class Factory : public pandora::AlgorithmFactory
+    {
+    public:
+        Algorithm *CreateAlgorithm() const;
+    };
+
+private:
+    StatusCode Run();
+    StatusCode ReadSettings(TiXmlHandle xmlHandle);
+
+    // Member variables here
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline pandora::Algorithm *TemplateAlgorithm::Factory::CreateAlgorithm() const
+{
+    return new TemplateAlgorithm();
+}
+
+#endif // #ifndef TEMPLATE_ALGORITHM_H
---
+++ src/Algorithms/TemplateAlgorithm.cc
@@ -0,0 +1,27 @@
+/**
+ *  @file   PandoraPFANew/src/Algorithms/TemplateAlgorithm.cc
+ * 
+ *  @brief  Implementation of the template algorithm class.
+ * 
+ *  $Log: $
+ */
+
+#include "Algorithms/TemplateAlgorithm.h"
+
+using namespace pandora;
+
+StatusCode TemplateAlgorithm::Run()
+{
+    // Algorithm code here
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode TemplateAlgorithm::ReadSettings(TiXmlHandle xmlHandle)
+{
+    // Read settings from xml file here
+
+    return STATUS_CODE_SUCCESS;
+}
