47a5360de75216e9abaf0b4bb397fe50e77e5df0
John Marshall <marshall@hep.phy.cam.ac.uk>
2009-09-04 13:55:07 +0000

    Added xml helper class.
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraPFANew/trunk@64 e861a63a-b6b5-47dc-a5de-b4304083765c

---
+++ include/Xml/XmlHelper.h
@@ -0,0 +1,27 @@
+/**
+ *  @file   PandoraPFANew/include/Xml/XmlHelper.h
+ * 
+ *  @brief  Header file for the xml helper class.
+ * 
+ *  $Log: $
+ */
+#ifndef XML_HELPER_H
+#define XML_HELPER_H 1
+
+#include "Xml/tinyxml.h"
+
+namespace pandora
+{
+
+/**
+ *  @brief  XmlHelper class
+ */
+class XmlHelper
+{
+public:
+
+};
+
+} // namespace pandora
+
+#endif // #ifndef XML_HELPER_H
---
+++ src/Xml/XmlHelper.cc
@@ -0,0 +1,14 @@
+/**
+ *  @file   PandoraPFANew/src/Xml/XmlHelper.cc
+ * 
+ *  @brief  Implementation of the xml helper class.
+ * 
+ *  $Log: $
+ */
+ 
+#include "Xml/XmlHelper.h"
+
+namespace pandora
+{
+
+} // namespace pandora
