5b45ff88fe799063aec16954a4810821b97e4772
John Marshall <marshall@hep.phy.cam.ac.uk>
2009-09-09 13:36:34 +0000

    Separate PandoraPFANew library from Marlin processor, SiD processor, test processor, etc.
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraPFANew/trunk@83 e861a63a-b6b5-47dc-a5de-b4304083765c

---
+++ BuildSetup.cmake
@@ -0,0 +1,39 @@
+#############################################################################
+# cmake build setup for PandoraPFANew
+#
+# For building PandoraPFA with cmake type:
+# (1) $ mkdir build
+# (2) $ cd build
+# (3) $ cmake -C ../BuildSetup.cmake ..
+# (4) $ make install
+#
+# @author Jan Engels, DESY
+#############################################################################
+
+
+#############################################################################
+# Setup path variables
+#############################################################################
+
+# Path to Pandora Monitoring
+SET( PANDORA_MONITORING_HOME "path_to_PandoraMonitoring"
+    CACHE PATH "Path to PandoraMonitoring" FORCE )
+
+###############################################
+# Project Options
+###############################################
+
+#SET( INSTALL_DOC OFF CACHE BOOL "Set to OFF to skip build/install Documentation" FORCE )
+
+# set cmake build type
+# possible options are: None Debug Release RelWithDebInfo MinSizeRel
+#SET( CMAKE_BUILD_TYPE "Debug" CACHE STRING "Choose the type of build" FORCE )
+
+###############################################
+# Advanced Options
+###############################################
+
+#SET( BUILD_SHARED_LIBS OFF CACHE BOOL "Set to OFF to build static libraries" FORCE )
+
+# installation path for PandoraPFA
+# SET( CMAKE_INSTALL_PREFIX "foo/bar" CACHE STRING "Where to install PandoraPFANew" FORCE )
