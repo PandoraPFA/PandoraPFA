7a7d55a702ccaf941d206b6d3f449cd4ca0a98ae
John Marshall <marshall@hep.phy.cam.ac.uk>
2009-09-09 16:42:51 +0000

    Allow PandoraPFANew to link against PandoraMonitoring.
    git-svn-id: https://svnsrv.desy.de/basic/PandoraPFANew/PandoraPFANew/trunk@86 e861a63a-b6b5-47dc-a5de-b4304083765c

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -1,7 +1,7 @@
 #############################################################################
 # cmake build setup for PandoraPFANew
 #
-# For building PandoraPFA with cmake type:
+# For building PandoraPFANew with cmake type:
 # (1) $ mkdir build
 # (2) $ cd build
 # (3) $ cmake -C ../BuildSetup.cmake ..
@@ -15,9 +15,19 @@
 # Setup path variables
 #############################################################################
 
-# Path to Pandora Monitoring
-#SET( PANDORA_MONITORING_HOME "path_to_PandoraMonitoring"
+# The three path variables below are only required if you wish to use the pandora monitoring package
+
+# PANDORA_HOME
+#SET( PANDORA_HOME "path_to_pandora_home"
+#    CACHE PATH "Path to pandora Software" FORCE )
+
+# Path to PandoraMonitoring
+#SET( PandoraMonitoring_HOME "${PANDORA_HOME}/PandoraMonitoring"
 #    CACHE PATH "Path to PandoraMonitoring" FORCE )
+
+# CMake Modules Path
+#SET( CMAKE_MODULE_PATH "${PANDORA_HOME}/CMakeModules/v01-08"
+#    CACHE PATH "Path to CMake Modules" FORCE )
 
 ###############################################
 # Project Options
